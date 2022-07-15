a896b61c3cefac03de1d502d3750b073c65ae461
Deri Kurniawan <deri.netuchi@gmail.com>
2022-07-15 14:31:33 +0700

    Fix(Layout): 🎨 Fix Login & Register Layout
    - Fix Responsive Register & Login Page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -11,91 +11,113 @@ const Login = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    return console.log(data);
+  };
 
   return (
     <>
       <Head title="Login" />
 
       <div className="grid grid-cols-1 grid-rows-1 h-screen md:grid-cols-2">
-        <div className="flex flex-col justify-center w-80 mx-auto p-6">
-          <div className="mb-4 md:mb-6">
-            <div className="mb-2 flex justify-center">
-              <Avatar img="https://res.cloudinary.com/ikram20/image/upload/v1657812999/dreambinar_brbdnc.jpg" />
-            </div>
-            <h1 className="font-[Inter] text-2xl font-bold text-center mb-2">
-              Sign in to your account
-            </h1>
-            <p className="text-center text-slate-400">
-              Not Have an Account?{" "}
-              <Link to="/register" className="text-blue-600">
-                Sign Up
-              </Link>
-            </p>
-          </div>
-
-          <form
-            className="flex flex-col gap-4"
-            onSubmit={handleSubmit(onSubmit)}
-          >
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="email" value="Email" />
+        <div className="py-6 px-6 md:py-0 md:px-10 lg:px-20 xl:px-40 my-auto">
+          <div className="flex flex-col box-border">
+            <div className="mb-3">
+              <div className="mb-2 flex justify-center">
+                <Avatar img="https://res.cloudinary.com/ikram20/image/upload/v1657812999/dreambinar_brbdnc.jpg" />
               </div>
-              <TextInput
-                id="email"
-                type="email"
-                {...register("email", { required: true })}
-              />
-              {errors.email && (
-                <p className="text-red-700 text-sm mt-2">
-                  * This field is required
-                </p>
-              )}
+              <h1 className="font-[Inter] text-2xl font-bold text-center mb-2">
+                Sign in to your account
+              </h1>
+              <p className="text-center text-slate-400">
+                Not Have an Account?{" "}
+                <Link to="/register" className="text-blue-600">
+                  Sign Up
+                </Link>
+              </p>
             </div>
-            <div>
-              <div className="mb-2 block">
-                <Label htmlFor="password" value="Password" />
-              </div>
-              <TextInput
-                id="password"
-                type="password"
-                {...register("password", { required: true })}
-              />
-              {errors.password && (
-                <p className="text-red-700 text-sm mt-2">
-                  * This field is required
-                </p>
-              )}
-            </div>
-            <button
-              type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-full dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-            >
-              Sign In
-            </button>
-          </form>
 
-          <div className="flex justify-center items-center text-sm mt-5">
-            <svg
-              className="w-4 h-4"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+            <form
+              className="flex flex-col gap-2"
+              onSubmit={handleSubmit(onSubmit)}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M10 19l-7-7m0 0l7-7m-7 7h18"
-              />
-            </svg>{" "}
-            <p className="ml-2">
-              <Link to="/" className="font-bold">
-                Back To Home
-              </Link>
-            </p>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="email" value="Email*" />
+                </div>
+                <TextInput
+                  color={errors.email && "failure"}
+                  id="email"
+                  type="email"
+                  {...register("email", { required: true })}
+                />
+                {errors.email && (
+                  <p className="text-red-700 text-sm pt-1">
+                    * This field is required
+                  </p>
+                )}
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="password" value="Password*" />
+                </div>
+                <TextInput
+                  color={errors.password && "failure"}
+                  id="password"
+                  type="password"
+                  {...register("password", { required: true })}
+                />
+                {errors.password && (
+                  <p className="text-red-700 text-sm pt-1">
+                    * This field is required
+                  </p>
+                )}
+              </div>
+
+              <div className="flex items-center my-2">
+                <TextInput
+                  id="remember"
+                  type="checkbox"
+                  color="primary"
+                  // color="primary"
+                  {...register("remember", { required: false })}
+                />
+                <label
+                  htmlFor="remember"
+                  className="ml-2 text-sm text-gray-900 dark:text-gray-300"
+                >
+                  Remember me
+                </label>
+              </div>
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-full dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Sign In
+              </button>
+            </form>
+
+            <div className="flex justify-center items-center text-sm mt-3">
+              <svg
+                className="w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                />
+              </svg>{" "}
+              <p className="ml-2">
+                <Link to="/" className="font-bold">
+                  Back To Home
+                </Link>
+              </p>
+            </div>
           </div>
         </div>
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,16 +1,158 @@
 import React from "react";
 import Head from "../components/Head";
+import { Link } from "react-router-dom";
+import { Avatar, Label, TextInput } from "flowbite-react";
+import { useForm } from "react-hook-form";
 
-const Register = () => {
+const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    return console.log(data);
+  };
+
   return (
     <>
       <Head title="Register" />
 
-      <article>
-        <h1>Register</h1>
-      </article>
+      <div className="grid grid-cols-1 grid-rows-1 h-screen md:grid-cols-2">
+        <div className="py-6 px-6 md:py-0 md:px-10 lg:px-20 xl:px-40 my-auto">
+          <div className="flex flex-col box-border">
+            <div className="mb-3">
+              <div className="mb-2 flex justify-center">
+                <Avatar img="https://res.cloudinary.com/ikram20/image/upload/v1657812999/dreambinar_brbdnc.jpg" />
+              </div>
+              <h1 className="font-[Inter] text-2xl font-bold text-center mb-2">
+                Create your account
+              </h1>
+              <p className="text-center text-slate-400">
+                Have an Account?{" "}
+                <Link to="/login" className="text-blue-600">
+                  Sign In
+                </Link>
+              </p>
+            </div>
+
+            <form
+              className="flex flex-col gap-2"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="name" value="Name*" />
+                </div>
+                <TextInput
+                  color={errors.name && "failure"}
+                  id="name"
+                  type="name"
+                  {...register("name", { required: true })}
+                />
+                {errors.name && (
+                  <p className="text-red-700 text-sm pt-1">
+                    * This field is required
+                  </p>
+                )}
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="email" value="Email*" />
+                </div>
+                <TextInput
+                  color={errors.email && "failure"}
+                  id="email"
+                  type="email"
+                  {...register("email", { required: true })}
+                />
+                {errors.email && (
+                  <p className="text-red-700 text-sm pt-1">
+                    * This field is required
+                  </p>
+                )}
+              </div>
+              <div>
+                <div className="mb-2 block">
+                  <Label htmlFor="password" value="Password*" />
+                </div>
+                <TextInput
+                  color={errors.password && "failure"}
+                  id="password"
+                  type="password"
+                  {...register("password", { required: true })}
+                />
+                {errors.password && (
+                  <p className="text-red-700 text-sm pt-1">
+                    * This field is required
+                  </p>
+                )}
+              </div>
+
+              <div className="flex flex-col my-2">
+                <div className="flex items-center">
+                  <TextInput
+                    id="terms"
+                    type="checkbox"
+                    color={errors.terms ? "failure" : "primary"}
+                    {...register("terms", { required: true })}
+                  />
+                  <label
+                    htmlFor="terms"
+                    className="ml-2 text-sm text-gray-900 dark:text-gray-300 hover:cursor-pointer"
+                  >
+                    I agree with the{" "}
+                    <Link to="/terms" className="text-blue-600 hover:underline">
+                      Terms and Conditions
+                    </Link>
+                  </label>
+                </div>
+                <div>
+                  {errors.terms && (
+                    <p className="text-red-700 text-sm pt-1">
+                      * You must agree to the terms and conditions
+                    </p>
+                  )}
+                  <div></div>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 w-full dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              >
+                Sign Up
+              </button>
+            </form>
+
+            <div className="flex justify-center items-center text-sm mt-3">
+              <svg
+                className="w-4 h-4"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M10 19l-7-7m0 0l7-7m-7 7h18"
+                />
+              </svg>{" "}
+              <p className="ml-2">
+                <Link to="/" className="font-bold">
+                  Back To Home
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-slate-200 hidden md:block"></div>
+      </div>
     </>
   );
 };
 
-export default Register;
+export default Login;
